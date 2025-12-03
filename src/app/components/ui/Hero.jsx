547793c2f976c18160b0bffcace67746c1f4d39e
Oscar <albertlund121@gmail.com>
2025-12-03 15:16:20 +0100

    style: adjust spacing and opacity in Hero component

--- src/app/components/ui/Hero.jsx
+++ src/app/components/ui/Hero.jsx
@@ -3,14 +3,14 @@ import Button from "./Button";
 
 export default function Hero() {
   return (
-    <div className="flex flex-col items-center gap-16">
+    <div className="flex flex-col items-center gap-8">
       <div className="mb-12">
         <Image
           src="/svg/dmp-logo-white.svg"
           alt="DMP Logo"
           width={200}
           height={200}
-          className="opacity-60"
+          className="opacity-50"
         />
       </div>
       <div className="">
