2548598606a0a9581e4f3c2fd1e6c468c7edc057
Oscar <albertlund121@gmail.com>
2025-12-04 12:30:20 +0100

    button

--- src/app/components/ui/Button.jsx
+++ src/app/components/ui/Button.jsx
@@ -1,8 +1,15 @@
-export default function Button({ children, variant = 'primary' }) {
+
+
+export default function Button({ children, variant = 'primary', size = 'md' }) {
   const variants = {
     primary: 'bg-[#4F649B] text-white hover:text-[#4F649B] hover:bg-white hover:underline hover:scale-175',
   };
 
+  const size = {
+    sm: 'px-4 py-2 text-sm',
+    md: 'px-6 py-3 text-base',
+    lg: 'px-8 py-4 text-lg',
+  };
   return (
     <button 
       className={`px-6 py-2.5 rounded-xl transition-all duration-300 ${variants[variant]}`}
@@ -12,3 +19,4 @@ export default function Button({ children, variant = 'primary' }) {
     </button>
   );
 }
+
