1f6d3b6c07629945e0aa742098e6615ebff40822
Sofie <Soov0001@stud.kea.dk>
2025-12-08 12:41:41 +0100

    h1 global rettelse

--- src/app/webshop/page.js
+++ src/app/webshop/page.js
@@ -1,9 +1,11 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import Navigation from '../components/layout/Navigation';
+import Link from "next/link";
+import Image from "next/image";
+import Navigation from "../components/layout/Navigation";
 
 async function getProducts() {
-  const res = await fetch('https://script.google.com/macros/s/AKfycbzvkDaCSaY8XlmCI14PxcahsWYTwLrRdOQVb29cBPCl4TJfDmbRmvP_B7E4N3C3YDMR/exec');
+  const res = await fetch(
+    "https://script.google.com/macros/s/AKfycbzvkDaCSaY8XlmCI14PxcahsWYTwLrRdOQVb29cBPCl4TJfDmbRmvP_B7E4N3C3YDMR/exec"
+  );
   const data = await res.json();
   return data;
 }
@@ -14,31 +16,23 @@ export default async function Webshop() {
   return (
     <div className=" bg-white">
       <Navigation />
-     
-        <h1>MERCH</h1>
-        
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          {products.map((product) => (
-            <Link 
-              key={product.id} 
-              href={`/webshop/${product.id}`}
-            >
-              <Image
-                src={product.thumbnail}
-                alt={product.name}
-                width={300}
-                height={300}
-            
-              />
-              <h3>
-                {product.name}
-              </h3>
-              <p>{product.description}</p>
 
-            </Link>
-          ))}
-        </div>
-     
+      <h1>MERCH</h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        {products.map((product) => (
+          <Link key={product.id} href={`/webshop/${product.id}`}>
+            <Image
+              src={product.thumbnail}
+              alt={product.name}
+              width={300}
+              height={300}
+            />
+            <h3>{product.name}</h3>
+            <p>{product.description}</p>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
