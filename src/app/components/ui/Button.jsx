21807d27b6c1938513c9be32ab2eb9c4782495cc
Sofie <Soov0001@stud.kea.dk>
2025-12-09 11:59:12 +0100

    button forside adjust scale

--- src/app/components/ui/Button.jsx
+++ src/app/components/ui/Button.jsx
@@ -1,9 +1,9 @@
 export default function Button({ children, variant = "secondary" }) {
   const variants = {
     primary:
-      "bg-[#4F649B] text-white hover:text-[#4F649B] hover:bg-white hover:underline hover:scale-175",
+      "bg-[#4F649B] text-white hover:text-[#4F649B] hover:bg-white hover:underline hover:scale-110",
     secondary:
-      "bg-white text-[#4F649B] hover:text-white hover:bg-[#4F649B] hover:underline hover:scale-175",
+      "bg-white text-[#4F649B] hover:text-white hover:bg-[#4F649B] hover:underline hover:scale-110",
   };
 
   return (
