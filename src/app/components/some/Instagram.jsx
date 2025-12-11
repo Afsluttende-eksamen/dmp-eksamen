0754bbba28806ccccb8afde79b8312df3f044b50
Sofie <Soov0001@stud.kea.dk>
2025-12-11 16:27:59 +0100

    youtube imbed og rettelser

--- src/app/components/some/Instagram.jsx
+++ src/app/components/some/Instagram.jsx
@@ -9,29 +9,31 @@ export default function InstagramEmbed({ posts }) {
         className="elfsight-app-0266d283-d73c-4075-9a78-a260545c7e23"
         data-elfsight-app-lazy
       ></div>
-
-      <div className="overflow-y-hidden w-full overflow-x-auto whitespace-nowrap scrollbar-hide flex gap-2.5 h-90">
-        <Script
-          src="https://www.instagram.com/embed.js"
-          strategy="lazyOnload"
-        />
-
-        {posts.map((url) => (
-          <blockquote
-            key={url}
-            className="relative overflow-hidden -top-15 instagram-media inline-block mr-4 h-97 pb-10  rounded-2xl "
-            data-instgrm-permalink={url}
-            data-instgrm-version="14"
-            style={{
-              width: "300px",
-              display: "inline-block",
-              background: "#fff",
-              border: 0,
-              margin: 0,
-              padding: 0,
-            }}
+      <div>
+        <h2>Instagram</h2>
+        <div className="overflow-y-hidden w-full overflow-x-auto whitespace-nowrap scrollbar-hide flex gap-2.5 h-90 ">
+          <Script
+            src="https://www.instagram.com/embed.js"
+            strategy="lazyOnload"
           />
-        ))}
+
+          {posts.map((url) => (
+            <blockquote
+              key={url}
+              className="relative overflow-hidden -top-15 instagram-media inline-block  mr-4 h-98 w-115 pb-10  rounded-2xl "
+              data-instgrm-permalink={url}
+              data-instgrm-version="14"
+              style={{
+                width: "600px",
+                display: "inline-block",
+                background: "#fff",
+                border: 0,
+                margin: 0,
+                padding: 0,
+              }}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
--- src/app/concerts/page.js
+++ src/app/concerts/page.js
@@ -3,6 +3,7 @@ import CartIcon from "../components/cart/CartIcon";
 import Instagram from "../components/some/Instagram";
 import InstagramEmbed from "../components/some/Instagram";
 import TikTokEmbed from "../components/some/Tiktok";
+import YouTubeFeed from "../components/some/Youtube";
 export default function Concerts() {
   return (
     <div>
@@ -24,6 +25,16 @@ export default function Concerts() {
           "https://www.tiktok.com/@dansermedpiger/video/7531763809195740438",
           "https://www.tiktok.com/@dansermedpiger/video/7527670616883727638",
           "https://www.tiktok.com/@dansermedpiger/video/7522852754495229206",
+        ]}
+      />
+
+      <YouTubeFeed
+        videos={[
+          "https://www.youtube.com/embed/7t0W4ANPOUU?si=zXg8nCpHMrU0q-0T",
+          "https://www.youtube.com/embed/47X1M9SYqSw?si=L4yqBHoao88Qh1Q0",
+          "https://www.youtube.com/embed/HIrmfSUZ4i4?si=0trDmgt3fyW894cG",
+          "https://www.youtube.com/embed/URbWuLjIYaQ?si=N_whjD0DODLwrhsz",
+          "https://www.youtube.com/embed/rRG8NDu-CEs?si=8AYM65mBo7H3o2Fh",
         ]}
       />
       <Footer></Footer>
