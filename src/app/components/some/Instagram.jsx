212a4ab06ae21e018a829248c4f03cd5c80bdd66
Sofie <Soov0001@stud.kea.dk>
2025-12-11 15:03:41 +0100

    forsøg m insta

---
+++ src/app/components/some/Instagram.jsx
@@ -0,0 +1,38 @@
+"use client";
+import Script from "next/script";
+
+export default function InstagramEmbed({ posts }) {
+  return (
+    <>
+      <script src="https://elfsightcdn.com/platform.js" async></script>
+      <div
+        className="elfsight-app-0266d283-d73c-4075-9a78-a260545c7e23"
+        data-elfsight-app-lazy
+      ></div>
+
+      <div className="overflow-y-hidden w-full overflow-x-auto whitespace-nowrap scrollbar-hide flex gap-2.5 h-80">
+        <Script
+          src="https://www.instagram.com/embed.js"
+          strategy="lazyOnload"
+        />
+
+        {posts.map((url) => (
+          <blockquote
+            key={url}
+            className="relative overflow-hidden -top-15 instagram-media inline-block mr-4 h-90 pb-10 -mb-2.5-9 "
+            data-instgrm-permalink={url}
+            data-instgrm-version="14"
+            style={{
+              width: "300px",
+              display: "inline-block",
+              background: "#fff",
+              border: 0,
+              margin: 0,
+              padding: 0,
+            }}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
--- src/app/concerts/page.js
+++ src/app/concerts/page.js
@@ -1,9 +1,20 @@
 import Footer from "../components/layout/Footer";
 import CartIcon from "../components/cart/CartIcon";
+import Instagram from "../components/some/Instagram";
+import InstagramEmbed from "../components/some/Instagram";
 export default function Concerts() {
   return (
     <div>
-      <CartIcon />
+      <InstagramEmbed
+        posts={[
+          "https://www.instagram.com/p/DRkHkAZjHap/",
+          "https://www.instagram.com/p/DLe8aEqsvrL/",
+          "https://www.instagram.com/p/DKulEPQiGq8/",
+          "https://www.instagram.com/p/DKKa3sfs4Or/",
+          "https://www.instagram.com/p/DJ4MWJ-sJF8/",
+          "https://www.instagram.com/p/DIv5cNrM7vU/",
+        ]}
+      />
       <Footer></Footer>
     </div>
   );
