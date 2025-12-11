c0eca3ed06de5f27f0b48a640e888fd50585c711
Sofie <Soov0001@stud.kea.dk>
2025-12-11 15:55:37 +0100

    tiktok embed forsøg

---
+++ src/app/components/some/Tiktok.jsx
@@ -0,0 +1,34 @@
+"use client";
+import Script from "next/script";
+
+export default function TikTokEmbed({ tiktokPosts }) {
+  return (
+    <div>
+      <Script src="https://www.tiktok.com/embed.js" strategy="lazyOnload" />
+      <div className="flex overflow-x-scroll w-full gap-2.5 p-4 whitespace-nowrap scrollbar-hide h-145 overflow-y-hidden ">
+        {tiktokPosts.map((url) => {
+          const videoId = url.split("/video/")[1];
+          return (
+            <blockquote
+              key={url}
+              className="relative -top-5  tiktok-embed mr-4 inline-block h-140 overflow-hidden pb-20"
+              cite={url}
+              data-video-id={videoId}
+              style={{
+                minWidth: "300px",
+
+                display: "inline-block",
+              }}
+            >
+              <section>
+                <a target="_blank" title="@dansermedpiger" href={url}>
+                  @dansermedpiger
+                </a>
+              </section>
+            </blockquote>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
--- src/app/concerts/page.js
+++ src/app/concerts/page.js
@@ -2,6 +2,7 @@ import Footer from "../components/layout/Footer";
 import CartIcon from "../components/cart/CartIcon";
 import Instagram from "../components/some/Instagram";
 import InstagramEmbed from "../components/some/Instagram";
+import TikTokEmbed from "../components/some/Tiktok";
 export default function Concerts() {
   return (
     <div>
@@ -13,6 +14,16 @@ export default function Concerts() {
           "https://www.instagram.com/p/DKKa3sfs4Or/",
           "https://www.instagram.com/p/DJ4MWJ-sJF8/",
           "https://www.instagram.com/p/DIv5cNrM7vU/",
+        ]}
+      />
+      <TikTokEmbed
+        tiktokPosts={[
+          "https://www.tiktok.com/@dansermedpiger/video/7581542355820432643",
+          "https://www.tiktok.com/@dansermedpiger/video/7579660453069016342",
+          "https://www.tiktok.com/@dansermedpiger/video/7546601530791660822",
+          "https://www.tiktok.com/@dansermedpiger/video/7531763809195740438",
+          "https://www.tiktok.com/@dansermedpiger/video/7527670616883727638",
+          "https://www.tiktok.com/@dansermedpiger/video/7522852754495229206",
         ]}
       />
       <Footer></Footer>
