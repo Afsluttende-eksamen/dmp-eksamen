070cb1a2e9976508bbea21f5b0455529ac255472
Sofie <Soov0001@stud.kea.dk>
2025-12-12 10:31:03 +0100

    spotify komponent some

---
+++ src/app/components/some/Spotify.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+export default function Spotify() {
+  return (
+    <div>
+      <h2>Spotify</h2>
+      <div className="flex overflow-x-auto gap-2.5 p-4 whitespace-nowrap scrollbar-hide">
+        <iframe
+          data-testid="embed-iframe"
+          style={{ borderRadius: "12px" }}
+          src="https://open.spotify.com/embed/artist/0OhwwB6b7ugDTS41UT4YJS?utm_source=generator"
+          width="50%"
+          height="500"
+          //   frameBorder="0"
+          //   allowfullscreen=""
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+          className="mx-auto"
+        ></iframe>
+      </div>
+    </div>
+  );
+}
