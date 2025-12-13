2cb104978ee6bed5bc173342fedc3d170afd0337
Sofie <Soov0001@stud.kea.dk>
2025-12-13 15:34:09 +0100

    mere

--- src/app/om-os/page.jsx
+++ src/app/om-os/page.jsx
@@ -2,6 +2,8 @@ import Navigation from "../components/layout/Navigation";
 import Image from "next/image";
 import Spotify from "../components/some/Spotify";
 import SoMeEmbed from "../components/some/SoMeEmbed";
+import Link from "next/link";
+import Button from "../components/ui/Button";
 
 export default function AboutPage() {
   return (
@@ -15,22 +17,40 @@ export default function AboutPage() {
         height={946}
         className="size-4/5 mx-auto"
       />
-      <p className="max-w-[65ch]">
+
+      <p className="max-w-[65ch] mx-auto">
         Vi er fem venner, der har delt kærligheden for musik siden barndommen.
         Vi voksede op med instrumenter i hænderne og et øvelokale, der hurtigt
         blev vores andet hjem. Gennem årene har venskabet været fundamentet for
         vores lyd - ærlig, energisk og fyldt med den glæde, der opstår, når man
         spiller musik med mennesker, man kender ud og ind. For os handler musik
         ikke kun om toner og tekster, men om fællesskab, nærvær og de øjeblikke,
-        der opstår mellem publikum og band. Vi laver musik, fordi vi ikke kan
-        lade være, og fordi det stadig føles lige så rigtigt, som da vi spillede
-        vores første akkorder sammen.{" "}
+        der opstår mellem publikum og band. Vi laver musik, fordi vi elsker det,
+        og fordi det stadig føles lige så rigtigt, som da vi spillede vores
+        første akkorder sammen.{" "}
       </p>
+
+      <Image
+        src="/svg/dmp2.svg"
+        alt="Description of image"
+        width={400}
+        height={370}
+        className="mx-auto my-8"
+      />
+
+      {/* <div className="text-center my-8">
+        <h4 className="py-4 ">Kom og fest med os!</h4>
+        <Button variant="primary" href="/koncerter">
+          Se koncerter
+        </Button>
+      </div> */}
+
+      <Spotify />
+
       <h3>Se hvad der sker bag scenen</h3>
       <div>
         <SoMeEmbed />
       </div>
-      <Spotify />
     </div>
   );
 }
