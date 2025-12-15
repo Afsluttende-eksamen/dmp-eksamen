075a032d5b7c6893a3d352fb52fd097c238ccdc1
Sofie <Soov0001@stud.kea.dk>
2025-12-15 19:37:35 +0100

    det samme

--- src/app/components/some/Instagram.jsx
+++ src/app/components/some/Instagram.jsx
@@ -19,7 +19,7 @@ export default function Instagram({ posts }) {
         >
           <h3 className="ml-2">Instagram</h3>
         </Link>
-        <div className="overflow-y-hidden w-full overflow-x-auto whitespace-nowrap scrollbar-hide flex gap-2.5 h-[40dvh] md:h-[45dvh] lg:h-[70dvh] pb-4">
+        <div className="overflow-y-hidden w-full overflow-x-auto whitespace-nowrap scrollbar-hide flex gap-2.5 h-[40dvh] md:h-[45dvh] lg:h-[47dvh] pb-4">
           <Script
             src="https://www.instagram.com/embed.js"
             strategy="lazyOnload"
@@ -29,7 +29,7 @@ export default function Instagram({ posts }) {
             <div className="contain-content rounded-2xl" key={url}>
               <blockquote
                 key={url}
-                className="relative overflow-hidden -top-15 instagram-media inline-block  mr-4 w-[30dvw] h-[50dvh] md:h-[55dvh] md:w-[40dvw] lg:h-[75dvh] lg:w-[60dvw] pb-20  rounded-2xl "
+                className="relative overflow-hidden -top-15 instagram-media inline-block  mr-4 w-[30dvw] h-[50dvh] md:h-[55dvh] md:w-[40dvw]  lg:w-[25dvw] pb-20  rounded-2xl "
                 data-instgrm-permalink={url}
                 data-instgrm-version="14"
                 style={{
