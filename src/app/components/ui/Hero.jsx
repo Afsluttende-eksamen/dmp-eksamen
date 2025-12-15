c7318beb16939d9e9f673ce19ffb9641982a7307
Sofie <Soov0001@stud.kea.dk>
2025-12-15 16:50:07 +0100

    link rettelse knap på forsiden t united stage

--- src/app/components/ui/Hero.jsx
+++ src/app/components/ui/Hero.jsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
-import { useEffect, useRef } from 'react';
-import { gsap } from 'gsap';
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
 import Image from "next/image";
 import Button from "./Button";
 
@@ -14,14 +14,14 @@ export default function Hero() {
       repeat: -1,
       yoyo: true,
       duration: 0.8,
-      ease: 'power1.inOut',
+      ease: "power1.inOut",
     });
   }, []);
 
   const scrollToNextSection = () => {
     window.scrollTo({
       top: window.innerHeight,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   };
 
@@ -37,7 +37,9 @@ export default function Hero() {
         />
       </div>
       <div>
-        <Button variant="secondary">BOOK VIA UNITED STAGE</Button>
+        <Link href="https://www.unitedstage.dk/artist/danser-med-piger/">
+          <Button variant="secondary">BOOK VIA UNITED STAGE</Button>
+        </Link>
       </div>
     </div>
   );
