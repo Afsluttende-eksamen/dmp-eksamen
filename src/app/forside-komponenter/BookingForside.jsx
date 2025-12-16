35053af1ecfd67a5ca24358833bd6a77f476d15e
Oscar <albertlund121@gmail.com>
2025-12-16 18:06:43 +0100

    flflf

--- src/app/forside-komponenter/BookingForside.jsx
+++ src/app/forside-komponenter/BookingForside.jsx
@@ -1,25 +1,29 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import Button from '../components/ui/Button';
-import BookingItem from './BookingItem';
-import Link from 'next/link';
+import Image from "next/image";
+import Button from "../components/ui/Button";
+import BookingItem from "./BookingItem";
+import Link from "next/link";
 
 export default function BookingForside() {
   return (
     <section className="bg-white grid md:grid-cols-2 gap-1 py-20 px-0 md:px-8">
-      <div className="flex items-center justify-center md:justify-start px-4 md:px-8">
+      <div className="flex items-center justify-center md:justify-start px-2 md:px-8">
         <div className="py-0 md:py-16">
           <h2 className="mb-8 md:mb-12 md:text-left text-center">
             BOOK DANSER MED PIGER TIL
           </h2>
-          <div className="flex flex-col pt-4 gap-6 mb-8">
+          <div className="flex flex-col px-4 pt-4 gap-6 mb-8">
             <BookingItem number="1" text="Koncerter og festivaler" />
             <BookingItem number="2" text="Firmafester og events" />
             <BookingItem number="3" text="Private arrangementer" />
           </div>
           <div className="flex justify-center md:justify-end">
-            <Link href="https://unitedstage.dk/artister/danser-med-piger/" target="_blank" rel="noreferrer">
+            <Link
+              href="https://unitedstage.dk/artister/danser-med-piger/"
+              target="_blank"
+              rel="noreferrer"
+            >
               <Button variant="primary">KONTAKT NU</Button>
             </Link>
           </div>
